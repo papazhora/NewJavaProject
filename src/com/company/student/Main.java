2ac4453679ab7c4316327c3c23cb228c78d7ac17
Egor <4156464@gmail.com>
2018-06-22 13:51:24 +0300

    1000

--- src/com/company/student/Main.java
+++ src/com/company/student/Main.java
@@ -1,4 +1,20 @@
 package com.company.student;
 
+/**
+ * Get, Set, Scanner
+ * Enter you name: Egor
+ * Enter you age: 43
+ * You name is Egor
+ * You age is 43
+ */
 public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+            student.setName();
+            student.setAge();
+
+            student.getName();
+            student.getAge();
+
+    }
 }
--- src/com/company/student/Student.java
+++ src/com/company/student/Student.java
@@ -1,4 +1,33 @@
 package com.company.student;
+import java.util.Scanner;
 
 public class Student {
+    private String name;
+    private int age;
+
+    public void setName() {
+        System.out.print("Enter you name: ");
+            nameAccsept();
+
+    }
+    public void setAge() {
+        System.out.print("Enter you age: ");
+            ageAccsept();
+
+    }
+    public void nameAccsept() {
+        Scanner scanner = new Scanner(System.in);
+            this.name = scanner.nextLine();
+    }
+    public void ageAccsept() {
+        Scanner scanner = new Scanner(System.in);
+            this.age = scanner.nextInt();
+    }
+    public void getName() {
+        System.out.println("You name is " + this.name);
+    }
+    public void getAge() {
+        System.out.print("You age is " + this.age);
+    }
+
 }

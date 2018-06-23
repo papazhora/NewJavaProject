5fcc47d8c2490e5f8fdcbd9438225e6373796fe7
papazhora <4156464@gmail.com>
2018-06-23 11:28:37 +0300

    7777

--- src/com/company/client/Client.java
+++ src/com/company/client/Client.java
@@ -1,6 +1,24 @@
 package com.company.client;
+import java.util.Scanner;
 
 public class Client {
-    private name;
-    private age;
+    private String name;
+    private  int age;
+
+    public Client(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void clientNameAgeOut() {
+        System.out.print("You name is " + this.name + "\n" + "You age is " + this.age);
+    }
+
+    public static String setName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter you name: ");
+        return scanner.nextLine();
+    }
+
+
 }
--- src/com/company/client/Main.java
+++ src/com/company/client/Main.java
@@ -1,7 +1,13 @@
 package com.company.client;
 
 public class Main {
+
     public static void main(String[] args) {
+        Client client = new Client(Client.setName(), 43);
+        client.clientNameAgeOut();
+
+
+
 
     }
 }
